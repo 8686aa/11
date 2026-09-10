316dbf3c583564874b303eb97b9d1774c4406ec1
8686aa <28299376@qq.com>
2026-09-11 01:00:09 +0800

    fix(radar): ??????? http://106.55.76.34:666/

--- StarRelay/StarRelayApp.swift
+++ StarRelay/StarRelayApp.swift
@@ -76,7 +76,7 @@ struct RootTabView: View {
 
 // MARK: - 内置雷达页（横屏内嵌浏览器）
 
-private let radarURL = URL(string: "http://129.204.47.191:666/")!
+private let radarURL = URL(string: "http://106.55.76.34:666/")!
 
 /// 供刷新按钮持有的 WebView 引用
 private final class WebBox {
